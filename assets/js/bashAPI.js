c7b623ed3e2d7ce609fb26fe6ba4c507d77c0e36
leo <1582405074@qq.com>
2021-08-16 15:09:43 +0800

    完成个人中心功能的开发

--- assets/js/bashAPI.js
+++ assets/js/bashAPI.js
@@ -4,7 +4,6 @@
 $.ajaxPrefilter(function(options) {
     //在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3008' + options.url
-    console.log(options.url);
 
     // 统一为有权限的接口，设置headers请求头
     // 添加判断，如果url地址有/my/则运行
@@ -19,7 +18,7 @@ $.ajaxPrefilter(function(options) {
         // console.log('执行了complete回调');
         // console.log(res);
         //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
-        if (res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+        if (res.responseJSON.code === 1) {
             //强制清空token
             localStorage.removeItem('token')
                 //强制跳转登录页
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,25 @@
 $(function() {
-    //调用getUserInfo获取用户基本信息
-    getUserInfo()
-})
+        //调用getUserInfo获取用户基本信息
+        getUserInfo()
 
-var layer = layui.layer
-    //点击按钮，实现退出功能
-$('#btnLogout').on('click', function() {
-    // console.log('ok');
-    //提示用户是否确认退出
-    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
-        //do something
-        // 1.清空本地存储中的token
-        localStorage.removeItem('token')
-            // 2.重新跳转到登录页面
-        location.href = '/login.html'
-            //关闭confirm询问框
-        layer.close(index);
-    });
-})
 
-//获取用户的基本信息
+        var layer = layui.layer
+            //点击按钮，实现退出功能
+        $('#btnLogout').on('click', function() {
+            // console.log('ok');
+            //提示用户是否确认退出
+            layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+                //do something
+                // 1.清空本地存储中的token
+                localStorage.removeItem('token')
+                    // 2.重新跳转到登录页面
+                location.href = '/login.html'
+                    //关闭confirm询问框
+                layer.close(index);
+            });
+        })
+    })
+    //获取用户的基本信息
 function getUserInfo() {
     $.ajax({
         method: 'GET',
@@ -71,7 +71,7 @@ function renderAvatar(user) {
     if (user.user_pic !== null) {
         //3.1渲染图片头像
         //如果不为空则优先显示图片头像
-        $('.layui-nav-img').attr('scr', user.user_pic).show()
+        $('.layui-nav-img').attr('src', user.user_pic).show()
         $('.text-avatar').hide()
     } else {
         // 3.2渲染文本头像
